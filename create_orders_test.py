f4f1da31a73343afd4e7d03465e7d0ae5464b21d
Kilzirg <141187201+Kilzirg@users.noreply.github.com>
2024-03-24 02:01:18 +0300

    Add files via upload

--- create_orders_test.py
+++ create_orders_test.py
@@ -2,6 +2,7 @@ import sender_stand_request
 import data
 
 
+# Михаил Евсеев, 14 когорта, Авто-Тест на создание заказа и получение заказа по треку с помощью API Яндекс Самокат.
 def test_get_order_track():
     track = sender_stand_request.create_order(data.order_data).json()['track']
     response = sender_stand_request.get_order_track(track)
